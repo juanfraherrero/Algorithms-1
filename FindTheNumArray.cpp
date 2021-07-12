345ffea71e62cf729907e25fd0909cf52a64e2e1
juanfraherrero <82331755+juanfraherrero@users.noreply.github.com>
2021-07-11 22:56:27 -0300

    Update and rename main.cpp to FindTheNumArray.cpp

--- FindTheNumArray.cpp
+++ FindTheNumArray.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 
 using namespace std;
-int buscar(int arr[],int inic,int fin, int nro);
+int searchnum(int arr[],int inic,int fin, int nro);
+
+
 int main()
 {
 
     int arr[]={2,3,4,5,6,7};
     int inic = 0;
-    int fin = 5;
-    int nro = 6;
-    for (int j = 0; j<6;j++)
-    {
-        cout << arr[j] << endl;
-    }
-    cout << endl << endl;
+    int size = 6;
+    int nro = 6; //it´s the number you are searching for
 
     int index;
-    index = buscar(arr, inic, fin, nro); //se busca nro en el arreglo pasado, te devuelve el indice en el arr del mismo, -1 si no lo encuentra
+    index = searchnum(arr, 0, size-1, nro); //search the number, retunr the index of the first apearrence. returns -1 if doesn´t exist the number in the array.
 
     cout << index ;
 
     return 0;
 }
 
-int buscar(int arr[],int inic,int fin, int nro)
+int searchnum(int arr[],int inic,int fin, int nro)
 {
     if (inic < fin)
     {
@@ -35,10 +32,10 @@ int buscar(int arr[],int inic,int fin, int nro)
         else
         {
             if (arr[mitad] < nro)
-                buscar(arr, mitad + 1, fin, nro);
+                searchnum(arr, mitad + 1, fin, nro);
             else
             {
-                buscar(arr, inic, mitad - 1, nro);
+                searchnum(arr, inic, mitad - 1, nro);
             }
         }
     }
