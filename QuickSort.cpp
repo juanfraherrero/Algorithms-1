6c33d2d885a576f2ae83a38454fae44b44d210e0
juanfraherrero <82331755+juanfraherrero@users.noreply.github.com>
2021-07-11 22:49:38 -0300

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,19 +3,15 @@
 using namespace std;
 void quicksort(int arr[], int inic, int fin);
 int sortbypivot(int arr[],int inic,int fin);
-/*
+
 int main()
 {
+    //initializing the array
     int arr[]={2,7,6,4,2,5};
     int inic = 0;
     int fin = 5;
 
-    for (int j = 0; j < 6;j++)
-    {
-        cout << arr[j] << endl;
-    }
-    cout << endl << endl;
-
+    //this function execute the quicksort
     quicksort(arr, inic, fin);
 
     for (int j = 0; j < 6;j++)
@@ -26,13 +22,15 @@ int main()
 
     return 0;
 }
-*/
+
 void quicksort(int arr[], int inic, int fin)
+    //the quicksort don´t have a combine part, this kind of combine section it kind be the function sortbypivot, but repeating that quicksort haven´t got a combine section.
 {
 
     if (inic < fin)
     {
         int index = sortbypivot(arr, inic, fin);
+        //divide
         quicksort(arr, inic, index - 1);
         quicksort(arr, index + 1, fin);
     }
