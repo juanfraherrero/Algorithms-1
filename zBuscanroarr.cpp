e7b9a15e84ac1517cc7834fc16c308f27f349026
juanfraherrero <82331755+juanfraherrero@users.noreply.github.com>
2021-07-11 22:50:41 -0300

    Add files via upload

---
+++ zBuscanroarr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr[]={2,7,6,4,2,5};
+    int inic = 0;
+    int fin = 5;
+
+
+    buscaarr(arr, inic, fin);
+
+    return 0;
+}
+
+void buscaarr(int arr[],int inic,int fin)
+{
+
+}
